c4e1f9302cfb2998588692960ae113537c6ce73f
Realven-0612 <anhnhoang1@gmail.com>
2024-08-21 21:04:45 +0700

    Fixing EffectActor

--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -12,9 +12,8 @@ UCLASS()
 class AURA_API AAuraEffectActor : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
+
+public:
 	AAuraEffectActor();
 
 	UFUNCTION()
@@ -23,9 +22,7 @@ public:
 	UFUNCTION()
 	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
 private:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<USphereComponent> Sphere;
